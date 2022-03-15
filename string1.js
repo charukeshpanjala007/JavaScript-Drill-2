c8a4ec96806be7f39b192084af3711fb5ecb84a5
charukeshpanjala007 <charukesh.panjala.19.1@mountblue.tech>
2022-03-15 14:01:10 +0530

    modified string1.js

--- string1.js
+++ string1.js
@@ -1,38 +1,41 @@
-module.exports = function (strings) {
-    output = []
-    if (strings != undefined && strings != null) {
-        for (let index = 0; index < strings.length; index++) {
-            let string = (strings[index])
-            let number
-            if (string !== undefined && string != null) {
-                if (string.startsWith("-")) {
-                    number = "-" + (string.slice(2, string.length))
-                } else {
-                    number = (string.slice(1, string.length))
-                }
-                number = number.replace(",", "");
-                if (number.endsWith(".")) {
-                    let stringIndex = number.length - 1
-                    while (stringIndex > 0) {
-                        if (number[stringIndex] === ".") {
-                            number[stringIndex] = ""
-                        } else {
-                            break;
-                        }
-                        stringIndex -= 1
-                    }
-                }
-                if (parseFloat(number) == number) {
-                    output.push(number)
-                } else {
-                    output.push(0)
-                }
-            }else{
-                output.push(0)
-            }
+const getNumber = (string) => {
+    let number = ""
+    if (string[0] == "-") {
+        number += "-" + string.slice(2, string.length)
+    } else {
+        number += string.slice(1, string.length)
+    }
+    while(number[number.length-1]==="."){
+        number = number.slice(0,number.length-1)
+    }
+    if(Number.parseFloat(number)== number){
+        return number
+    }else{
+        return 0
+    }
+}
+
+const convertStringToNumber = (string) => {
+    if (string) {
+        return getNumber(string)
+
+    } else {
+        return 0
+    }
+}
+
+
+const stringProblem1 = (string) => {
+    let output = []
+    if (Array.isArray(string)) {
+        for (let index = 0; index < string.length; index++) {
+            let number = convertStringToNumber(string[index])
+            output.push(number)
         }
     } else {
-        output.push(0)
+        output = convertStringToNumber(string)
     }
-    return (output)
+    return output
 }
+
+module.exports = stringProblem1
